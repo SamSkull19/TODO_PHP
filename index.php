2922356d79710975e08d64e2a90c1bd14d986ce2
sifat-samin <sifatsamin19@gmail.com>
2025-10-28 21:24:30 +0600

    File separated

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TO DO</title>
-
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
+<?php include "header.php"; ?>
 
 <body>
     <section class="max-w-[1180px] mx-auto mt-20">
@@ -22,33 +13,9 @@
         </div>
 
         <div class="p-5 mt-10">
-            <table class="border-2 border-gray-700 border-collapse w-full text-left text-lg rounded-lg">
-                <thead>
-                    <tr class="bg-gray-100">
-                        <th class="border-2 border-gray-700 px-4 py-2">No.</th>
-                        <th class="border-2 border-gray-700 px-4 py-2">Name</th>
-                        <th class="border-2 border-gray-700 px-4 py-2">Action</th>
-                        <th class="border-2 border-gray-700 px-4 py-2">Modify</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td class="border-2 border-gray-700 px-4 py-2">1</td>
-                        <td class="border-2 border-gray-700 px-4 py-2">Sifat Samin</td>
-                        <td class="border-2 border-gray-700 px-4 py-2">
-                            <button class="bg-red-300 px-7 py-2 border border-red-950 text-base rounded-lg font-semibold">DELETE</button>
-                        </td>
-                        <td class="border-2 border-gray-700 px-4 py-2">
-                            <button class="bg-blue-300 px-7 py-2 border border-blue-950 text-base rounded-lg font-semibold">UPDATE</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <?php require "task_table.php"; ?>
         </div>
-
-
     </section>
 
 </body>
-
 </html>
---
+++ task_table.php
@@ -0,0 +1,22 @@
+<table class="border-2 border-gray-700 border-collapse w-full text-left text-lg rounded-lg">
+    <thead>
+        <tr class="bg-gray-100">
+            <th class="border-2 border-gray-700 px-4 py-2">No.</th>
+            <th class="border-2 border-gray-700 px-4 py-2">Name</th>
+            <th class="border-2 border-gray-700 px-4 py-2">Action</th>
+            <th class="border-2 border-gray-700 px-4 py-2">Modify</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td class="border-2 border-gray-700 px-4 py-2">1</td>
+            <td class="border-2 border-gray-700 px-4 py-2">Sifat Samin</td>
+            <td class="border-2 border-gray-700 px-4 py-2">
+                <button class="bg-red-300 px-7 py-2 border border-red-950 text-base rounded-lg font-semibold">DELETE</button>
+            </td>
+            <td class="border-2 border-gray-700 px-4 py-2">
+                <button class="bg-blue-300 px-7 py-2 border border-blue-950 text-base rounded-lg font-semibold">UPDATE</button>
+            </td>
+        </tr>
+    </tbody>
+</table>
