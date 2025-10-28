e76eb70f750f6f1e640c4c597333e834ba4345e7
sifat-samin <sifatsamin19@gmail.com>
2025-10-28 21:20:11 +0600

    Table and Inputs added

--- conn.php
+++ conn.php
@@ -7,7 +7,7 @@
 
         $conn = new PDO("mysql: host=$host; dbname=$dbname", $username, $password);
 
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE__EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     catch(PDOException $e){
         echo "Error is : " . $e->getMessage();
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,47 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body>
-    <h2 class="text-xl text-red-600">Samin</h2>
+    <section class="max-w-[1180px] mx-auto mt-20">
+        <div class="border-b-1 border-blue-950 ">
+            <h2 class="text-4xl mb-10 text-blue-900 font-semibold">Enter Your Tasks</h2>
+            <div class="mb-10">
+                <form action="">
+                    <input type="text" placeholder="Your Tasks" class="text-xl p-4 border border-blue-800 rounded-lg">
+                    <input type="submit" placeholder="Your Tasks" class="text-xl p-4 px-10 border border-blue-800 rounded-lg bg-cyan-900 text-gray-100">
+                </form>
+            </div>
+        </div>
+
+        <div class="p-5 mt-10">
+            <table class="border-2 border-gray-700 border-collapse w-full text-left text-lg rounded-lg">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="border-2 border-gray-700 px-4 py-2">No.</th>
+                        <th class="border-2 border-gray-700 px-4 py-2">Name</th>
+                        <th class="border-2 border-gray-700 px-4 py-2">Action</th>
+                        <th class="border-2 border-gray-700 px-4 py-2">Modify</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="border-2 border-gray-700 px-4 py-2">1</td>
+                        <td class="border-2 border-gray-700 px-4 py-2">Sifat Samin</td>
+                        <td class="border-2 border-gray-700 px-4 py-2">
+                            <button class="bg-red-300 px-7 py-2 border border-red-950 text-base rounded-lg font-semibold">DELETE</button>
+                        </td>
+                        <td class="border-2 border-gray-700 px-4 py-2">
+                            <button class="bg-blue-300 px-7 py-2 border border-blue-950 text-base rounded-lg font-semibold">UPDATE</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+    </section>
+
 </body>
+
 </html>
